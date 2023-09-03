c098cfe1d93401a3e4e2f61e136c474af1dcf3d0
shabbirshuvo <shabbir.unirostock@gmail.com>
2023-09-03 21:01:03 +0200

    working code with file opener using kivy

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from tkinter import filedialog
-
-from kivy.clock import Clock
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.uix.dialog import MDDialog
 from kivymd.app import MDApp
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+from kivy.core.window import Window
 from PIL import Image
 from pytesseract import pytesseract
-from kivy.core.window import Window
-from tkinter.filedialog import askopenfile
 
 Window.size = (500, 500)
 
@@ -18,7 +17,6 @@ class ExtractingTextApp(MDApp):
     def __init__(self):
         super().__init__()
         self.image_file = None
-        self.file = None
         self.extract_button = None
         self.image_text = None
         self.choose_button = None
@@ -26,23 +24,28 @@ class ExtractingTextApp(MDApp):
         self.title = "Extracting Text App"
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_manager,
+            select_path=self.select_path,
+        )
 
     def move_label(self, obj):
         text = self.location_label.text
         updated_text = text[1:] + text[0]
         self.location_label.text = updated_text
 
-    def choose_image(self, obj):
-        self.file = filedialog.askopenfile(
-        title="Choose an Image..",
-        mode="r",
-        filetypes=[("Image Files", "*.jpg *.png *.jpeg")]
-        )
-        self.image_file = self.file.name
+    def open_file_manager(self, obj):
+        self.file_manager.show('/')
+
+    def select_path(self, path):
+        self.file_manager.close()
+        self.image_file = path
         self.location_label.text = self.image_file
         self.extract_button.disabled = False
         self.choose_button.disabled = True
 
+    def exit_manager(self, *args):
+        self.file_manager.close()
 
     def extract_text(self, obj):
         pass
@@ -77,7 +80,7 @@ class ExtractingTextApp(MDApp):
             width=100,
             background_color=(1, .5, 0),
             bold=True,
-            on_press=self.choose_image,
+            on_press=self.open_file_manager,
             disabled=False,
         )
         self.root.add_widget(self.choose_button)
